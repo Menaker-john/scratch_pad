a4f52d6d2b235405858ee5bfebfb98df997ab028
John <menaker.john@outlook.com>
2022-09-21 20:18:26 -0500

    Implemented removing notes when closed

--- src/my_app.rs
+++ src/my_app.rs
@@ -1,5 +1,5 @@
 use eframe::{
-    egui::{CentralPanel, Context, Id, ScrollArea, SidePanel, TopBottomPanel, Window},
+    egui::{CentralPanel, Context, Id, TextEdit, TopBottomPanel, Window},
     get_value, set_value, App, CreationContext, Storage, APP_KEY,
 };
 use serde::{Deserialize, Serialize};
@@ -28,27 +28,27 @@ impl Default for Note {
 #[derive(Serialize, Deserialize)]
 pub struct MyApp {
     notes: Vec<Note>,
-    testing: String,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
-        Self {
-            notes: Vec::new(),
-            testing: "Default".to_string(),
-        }
+        Self { notes: Vec::new() }
     }
 }
 
 impl App for MyApp {
     fn save(&mut self, storage: &mut dyn Storage) {
-        self.testing = "Saved".to_string();
         set_value(storage, APP_KEY, self);
     }
 
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        for i in (0..self.notes.len()).rev() {
+            if !self.notes[i].open {
+                self.notes.remove(i);
+            }
+        }
+
         self.top_panel(ctx);
-        self.left_side_panel(ctx);
         self.center_panel(ctx);
     }
 }
@@ -73,39 +73,29 @@ impl MyApp {
         });
     }
 
-    // Far from finished
-    fn left_side_panel(&mut self, ctx: &Context) {
-        SidePanel::left("notes_list")
-            .resizable(false)
-            .show(ctx, |ui| {
-                ui.add_space(PADDING);
-
-                let scroll_area = ScrollArea::vertical();
-                ScrollArea::auto_shrink(scroll_area, [false; 2]).show(ui, |ui| {
-                    for note in self.notes.iter_mut() {
-                        ui.add_space(PADDING / 2.0);
-                        ui.horizontal(|ui| {
-                            ui.button(note.name.to_string());
-                            ui.button("Delete");
-                        });
-                    }
-                });
-            });
-    }
-
     fn center_panel(&mut self, ctx: &Context) {
         CentralPanel::default().show(ctx, |_| {
             for note in self.notes.iter_mut() {
-                let title = note.name.to_string();
+                let title = Self::truncate_title(note.name.to_string());
+
                 Window::new(title)
                     .id(note.id)
                     .open(&mut note.open)
+                    .resizable(true)
                     .show(ctx, |ui| {
                         ui.text_edit_singleline(&mut note.name);
                         ui.add_space(PADDING / 2.0);
-                        ui.text_edit_multiline(&mut note.content);
+                        ui.add_sized(ui.available_size(), TextEdit::multiline(&mut note.content));
                     });
             }
         });
+    }
+
+    fn truncate_title(title: String) -> String {
+        if title.len() > 9 {
+            return format!("{}...", title.chars().take(9).collect::<String>());
+        }
+
+        title
     }
 }
